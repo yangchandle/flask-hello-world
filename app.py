7fcc40b1bdba9184dc8e03cc40ec0c6dfe595e6c
yangchandle <xuchandle@gmail.com>
2017-05-17 20:05:10 +0800

    learn the debugger

--- app.py
+++ app.py
@@ -6,14 +6,29 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
 @app.route("/hello")
 
-#define the view using a function, which returns a string 
+# define the view using a function, which returns a string
 def hello_world():
-	return "hello, world!"
+	return "Hello, world123"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name)
+	else:
+		return "Not found", 404
 # start the development server using the run() method
 if __name__ == "__main__":
 	app.run()
