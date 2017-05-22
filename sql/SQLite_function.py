0f71e1426e81d012591f7ba2d838a30c80996c45
yangchandle <xuchandle@gmail.com>
2017-05-22 20:24:45 +0800

    create sql function.py

---
+++ sql/SQLite_function.py
@@ -0,0 +1,26 @@
+# SQLite Functions
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+
+    c = connection.cursor()
+    
+    # create a dictonary of sql queries
+	sql = {'acerage': "SELECT avg(population) FROM population",
+			'maximum': "SELECT max(population) FROM population",
+			'minimum': "SELECT min(population) FROM population",
+			'sum': "SELECT sum(population) FROM population",
+			'count': "SELECT count(city) FROM population"}
+			
+	# run each sql query item in the dictionary
+	for keys, values in sqli.items():
+		
+		# run sql
+		c.execute(values)
+		
+		# fetchone() retrieves one reord from the query 
+		results = c.fethchone()
+		
+		# output the result to sreen
+		print(keys + ":", results[0])
