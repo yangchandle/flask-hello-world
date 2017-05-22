93650de99399594bfe7c924aa228fcfa29e66b3e
yangchandle <xuchandle>
2017-05-22 21:01:41 +0800

    change the SQLite_function.py

--- sql/SQLite_function.py
+++ sql/SQLite_function.py
@@ -7,14 +7,14 @@ with sqlite3.connect("new.db") as connection:
     c = connection.cursor()
     
     # create a dictonary of sql queries
-	sql = {'acerage': "SELECT avg(population) FROM population",
+	sql = {'average': "SELECT avg(population) FROM population",
 			'maximum': "SELECT max(population) FROM population",
 			'minimum': "SELECT min(population) FROM population",
 			'sum': "SELECT sum(population) FROM population",
 			'count': "SELECT count(city) FROM population"}
 			
 	# run each sql query item in the dictionary
-	for keys, values in sqli.items():
+	for keys, values in sql.items():
 		
 		# run sql
 		c.execute(values)
