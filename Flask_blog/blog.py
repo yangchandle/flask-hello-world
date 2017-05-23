4479cc38ce7686d82aee531fcdde69253bba03d0
yangchandle <xuchandle>
2017-05-23 09:13:35 +0800

    first flask blog

---
+++ Flask_blog/blog.py
@@ -0,0 +1,29 @@
+# blog.py - controller
+
+# imports
+from flask import Flask, render_template, request, session, flask, redirect, url_for, g
+
+import sqlite3
+
+# configuration
+DATABASE = 'blog.db'
+
+app = FLask(__name__)
+
+# pulls in app configuration by looking for UPPERCASE varibles
+app.config.from_object(__name__)
+
+# function used for connecting to the database
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+@app.route("/")
+def login():
+	return render_template('login.html')
+
+@app.route("/main")
+def main():
+	return render_template('main.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
