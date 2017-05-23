e476e5d5e8a5d75b66858d67a75f02f266846eb4
yangchandle <xuchandle>
2017-05-23 10:44:55 +0800

    first day learn flask

--- Flask_blog/blog.py
+++ Flask_blog/blog.py
@@ -1,25 +1,49 @@
+# !/user/bin/env python
 # blog.py - controller
 
 # imports
-from flask import Flask, render_template, request, session, flask, redirect, url_for, g
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 
 import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
-app = FLask(__name__)
+app = Flask(__name__)
 
 # pulls in app configuration by looking for UPPERCASE varibles
 app.config.from_object(__name__)
+
+
+
 
 # function used for connecting to the database
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	error = None
+	status_code = 200
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or \
+			request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try agian.'
+			status_code = 401
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('main'))
+	return render_template('login.html', error=error), status_code
+	
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
 
 @app.route("/main")
 def main():
